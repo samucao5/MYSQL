8ffc0b7b04f16d1a42722c186cee4d6c45f73f5f
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-01 00:40:21 -0300

    Add files via upload

---
+++ udemy_mySQL/avançando_em_SELECT/ordenando_as_consultas/order_by.sql
@@ -0,0 +1,5 @@
+/*ORDER BY: é uma instrução para ordenação de resultados*/
+/*ASC: forma ascendente*/
+SELECT emp_no, salary, from_date FROM salaries ORDER BY salary ASC LIMIT 5;
+/*DESC: forma descendente*/
+SELECT emp_no, salary, from_date FROM salaries ORDER BY salary DESC LIMIT 5;
