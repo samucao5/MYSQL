c7148852c4093a8e22af9076bcc74e1fab786d07
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-31 23:32:46 -0300

    Add files via upload

---
+++ udemy_mySQL/avançando_em_SELECT/Distinct/Distinct.sql
@@ -0,0 +1,6 @@
+USE employees;
+/*o Distinct vai selecionar apenas as variações de valores*/
+/*SELECT DISTINCT <coluna> FROM <tabela>;*/
+SELECT * FROM employees;
+
+SELECT DISTINCT gender FROM employees;
