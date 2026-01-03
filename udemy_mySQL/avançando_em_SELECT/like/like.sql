9513b17663f6907cf49233ebccd0acd93ea85697
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-03 00:45:33 -0300

    Add files via upload

---
+++ udemy_mySQL/avançando_em_SELECT/like/like.sql
@@ -0,0 +1,13 @@
+/*LIKE: ele tem a premissa de filtra ainda mais nossos resultados*/
+/* LIKE '% palavra': ele procura no final da palavra*/
+SELECT * FROM employees WHERE first_name LIKE '%a';
+
+/* LIKE 'palavra %': ele procura no começo da palavra*/
+SELECT * FROM employees WHERE first_name LIKE 'sam%';
+
+/* LIKE '% palavra %': tanto no começo como no final da palavra*/
+SELECT * FROM employees WHERE first_name LIKE '%samu%';
+
+/*podemos usar com o count*/
+
+SELECT COUNT(*) AS nome_que_tem_pe FROM employees WHERE first_name LIKE '%pe%';
