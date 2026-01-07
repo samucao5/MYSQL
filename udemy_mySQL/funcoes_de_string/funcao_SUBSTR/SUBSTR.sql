f179d02c720e35b333818acf772a0385d18b1a7e
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2026-01-06 22:07:15 -0300

    Delete udemy_mySQL/funcoes_de_string/funcao_SUBSTR directory

--- udemy_mySQL/funcoes_de_string/funcao_SUBSTR/SUBSTR.sql
+++
@@ -1,9 +0,0 @@
-USE sakila;
-/*SUBSTR: extrai uma string a partir de dois indices, inicio e fim*/
--- SUBSTR(<texto>, <inicio>, <fim>);
-
-SELECT last_update, SUBSTR(last_update, 1, 4) AS ano,
- SUBSTR(last_update,6,2) AS mes,
- SUBSTR(last_update,9,2) AS dia,
- SUBSTR(last_update,12,8) AS horas
- FROM actor;
